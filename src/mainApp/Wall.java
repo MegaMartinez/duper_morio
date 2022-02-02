d9dba1e3ef501c03718bf04580844059b3e44d52
satkosb <satkosb@rose-hulman.edu>
2022-02-02 13:56:17 -0500

    Brennan, Created constructor and method for the wall class

--- src/mainApp/Wall.java
+++ src/mainApp/Wall.java
@@ -1,13 +1,25 @@
 package mainApp;
 
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.Rectangle2D.Double;
+
 public class Wall {
 
-	private final double X_COORD = 150;
-	private final double Y_COORD = 150;
+	private double xPos;
+	private double yPos;
 	private final double LENGTH = 100;
 	private final double WIDTH = 50;
+	private Rectangle2D.Double rect;
 	
+	public Wall(int x, int y) {
+		this.xPos = x;
+		this.yPos = y;
+		this.rect = new Rectangle2D.Double(xPos, yPos, this.LENGTH, this.WIDTH);
+	}
 	
+	private boolean collides(Rectangle2D hero) {
+		return this.rect.contains(hero);
+	}
 	
 	
 }
