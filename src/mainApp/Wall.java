e85b8612deb50d04d0f47f2aa3684217c3486df3
satkosb <satkosb@rose-hulman.edu>
2022-02-02 14:30:05 -0500

    wall class remade

--- src/mainApp/Wall.java
+++ src/mainApp/Wall.java
@@ -1,15 +1,23 @@
 package mainApp;
 
+import java.awt.geom.Rectangle2D;
+
 public class Wall {
 
-	private final double X_COORD = 150;
-	private final double Y_COORD = 150;
+	private double xPos;
+	private double yPos;
 	private final double LENGTH = 100;
 	private final double WIDTH = 50;
+	private Rectangle2D.Double rect;
 	
-	public Wall() {
-		
+	public Wall(int x, int y) {
+		this.xPos = x;
+		this.yPos = y;
+		this.rect = new Rectangle2D.Double(this.xPos, this.yPos, this.LENGTH, this.WIDTH);
 	}
 	
+	public boolean collision(Rectangle2D.Double hero) {
+		return this.rect.contains(hero);
+	}
 	
 }
