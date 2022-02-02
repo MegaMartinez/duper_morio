11e26eb6353839375d3a006d877bbaa24f375763
nunemajo <nunemajo@rose-hulman.edu>
2022-02-01 20:07:41 -0500

    Added a lot of basic JFrame stuff to Main and a barebones listener.

--- src/mainApp/MainApp.java
+++ src/mainApp/MainApp.java
@@ -1,5 +1,12 @@
 package mainApp;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.Timer;
 
 /**
  * Class: MainApp
@@ -9,10 +16,26 @@ package mainApp;
  */
 public class MainApp {
 	
+	public static final int TICK_DELAY = 40;
+	
 	
 	private void runApp() {
 		System.out.println("Write your cool arcade game here!");
 		System.out.println("test");
+		
+		JFrame frame = new JFrame("Old Duper Morio Cousins");
+		frame.setSize(500, 500);
+		
+		GameComponent gameComponent = new GameComponent();
+		
+		TickAdvanceListener tickAdvanceListener = new TickAdvanceListener(gameComponent);
+
+		Timer timer = new Timer(TICK_DELAY, tickAdvanceListener);
+		timer.start();
+		
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 	} // runApp
 
 	/**
