484b8ee74fc5daed71f5f1806325b4a7215e78c9
nunemajo <nunemajo@rose-hulman.edu>
2022-02-09 22:12:37 -0500

    In progress attempt at horizontal collision.

--- src/mainApp/Character.java
+++ src/mainApp/Character.java
@@ -2,6 +2,7 @@ package mainApp;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
 
 public abstract class Character {
@@ -49,21 +50,51 @@ public abstract class Character {
 	}
 	
 	public void platformCollide(Wall wall) {
+//		if(this.rect.intersects(wall.rect)) {
+//			if(this.rect.y + this.rect.height > wall.yPos && this.rect.y + this.rect.height < wall.yPos + 25) {
+//				this.yVelocity = 0;
+////				this.yPos = wall.yPos - this.rect.height;
+//				this.rect.y = wall.yPos - this.rect.height;
+//				this.isFalling = false;
+//			} else if(this.rect.y < wall.yPos + wall.rect.height) {
+//				this.yVelocity = 0;
+//				this.rect.y = wall.yPos + wall.rect.height;
+//				this.isFalling = true;
+//			}
+//		} else {
+//			this.isFalling = true;
+//		}
+//		if(this.rect.intersects(wall.rect)) {
+//			if(this.isFalling && this.rect.x + this.rect.width > wall.xPos) {
+//				this.xVelocity = 0;
+//				this.rect.x = wall.xPos - this.rect.width;
+//			} else if(this.isFalling && this.rect.x < wall.xPos + wall.rect.width) {
+//				this.xVelocity = 0;
+//				this.rect.x = wall.xPos + wall.rect.width;
+//			}
+//		}
+		
 		if(this.rect.intersects(wall.rect)) {
-			if(this.rect.y <= wall.yPos) {
+			if(this.rect.intersectsLine(wall.edges.get(0))) {
 				this.yVelocity = 0;
-//				this.yPos = wall.yPos - this.rect.height;
 				this.rect.y = wall.yPos - this.rect.height;
 				this.isFalling = false;
-			} else if(this.rect.y >= wall.yPos) {
+			} else if(this.rect.intersectsLine(wall.edges.get(2))) {
 				this.yVelocity = 0;
 				this.rect.y = wall.yPos + wall.rect.height;
 				this.isFalling = true;
 			}
-			
-		}
-		else {
+		} else {
 			this.isFalling = true;
+		}
+		if(this.rect.intersects(wall.rect)) {
+			if(this.rect.intersectsLine(wall.edges.get(1))) {
+				this.xVelocity = 0;
+				this.rect.x = wall.xPos - this.rect.width;
+			} else if(this.rect.intersectsLine(wall.edges.get(3))) {
+				this.xVelocity = 0;
+				this.rect.x = wall.xPos + wall.rect.width;
+			}
 		}
 	}
 }
--- src/mainApp/Wall.java
+++ src/mainApp/Wall.java
@@ -3,20 +3,28 @@ package mainApp;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.awt.geom.Line2D;
 
 public class Wall {
 	
-	private static final double LENGTH = 50;
-	private static final double WIDTH = 50;
+	private static final double WALL_LENGTH = 50;
+	private static final double WALL_HEIGHT = 50;
 
 	protected double xPos;
 	protected double yPos;
 	protected Rectangle2D.Double rect;
+	protected ArrayList<Line2D.Double> edges;
 	
 	public Wall(int x, int y) {
 		this.xPos = x;
 		this.yPos = y;
-		this.rect = new Rectangle2D.Double(this.xPos, this.yPos, this.LENGTH, this.WIDTH);
+		this.rect = new Rectangle2D.Double(this.xPos, this.yPos, WALL_LENGTH, WALL_HEIGHT);
+		this.edges = new ArrayList<Line2D.Double>();
+		this.edges.add(new Line2D.Double(x, y, x + WALL_LENGTH, y));
+		this.edges.add(new Line2D.Double(x, y, x, y + WALL_HEIGHT));
+		this.edges.add(new Line2D.Double(x, y + WALL_HEIGHT, x + WALL_LENGTH, y + WALL_HEIGHT));
+		this.edges.add(new Line2D.Double(x + WALL_LENGTH, y, x + WALL_LENGTH, y + WALL_HEIGHT));
 	}
 	
 	public boolean collision(Rectangle2D.Double hero) {
