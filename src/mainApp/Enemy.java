43f008f640e53e8f7a7b882c54de44d1c8f73170
nunemajo <nunemajo@rose-hulman.edu>
2022-02-13 20:25:40 -0500

    Enemies now turn around at walls and there's a jumper enemy class.

--- src/mainApp/Enemy.java
+++ src/mainApp/Enemy.java
@@ -1,6 +1,7 @@
 package mainApp;
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
 
 public class Enemy extends Character {
 	
@@ -15,6 +16,7 @@ public class Enemy extends Character {
 	public Enemy(double x, double y) {
 		super(x, y, ENEMY_WIDTH, ENEMY_HEIGHT);
 		this.xVelocity = HORI_SPEED;
+		this.yVelocity = 0;
 	}
 	
 	public Enemy(double x, double y, String type) {
@@ -40,6 +42,42 @@ public class Enemy extends Character {
 	public void collides(Hero hero) {
 		if (this.rect.intersects(hero.rect)) {
 			hero.respawn();
+		}
+	}
+	
+	@Override
+	public void update() {
+		super.update();
+		if(this.xPos == 50 || this.xPos == 650) {
+			this.xVelocity = -this.xVelocity;
+		}
+	}
+	
+	@Override
+	public void platformCollide(Wall wall) {
+		Rectangle2D.Double measureBox = new Rectangle2D.Double(this.rect.x + this.xVelocity, this.rect.y, this.rect.width, this.rect.height);
+		if(measureBox.intersects(wall.rect)) {
+			if(measureBox.x + measureBox.width >= wall.xPos && measureBox.x + measureBox.width < wall.xPos + wall.rect.width) {
+				this.xPos = wall.xPos - this.rect.width;
+			} else {
+				this.xPos = wall.xPos + wall.rect.width;
+			}
+			measureBox.x -= this.xVelocity;
+			this.xVelocity = -this.xVelocity;
+		} else {
+			measureBox.x -= this.xVelocity;
+		} measureBox.y += this.yVelocity;
+		if(measureBox.intersects(wall.rect)) {
+			if(measureBox.y + measureBox.height >= wall.yPos && measureBox.y + measureBox.height < wall.yPos + wall.rect.height) {
+				this.yPos = wall.yPos - this.rect.height;
+				this.isFalling = false;
+			} else {
+				this.yPos = wall.yPos + wall.rect.height;
+				this.isFalling = true;
+			}
+			this.yVelocity = 0;
+		} else {
+			this.isFalling = true;
 		}
 	}
 	
