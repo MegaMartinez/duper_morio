85180e52b8ef4325c1215c9de30147e42cd1f855
nunemajo <nunemajo@rose-hulman.edu>
2022-02-17 21:59:09 -0500

    Attempting to add projectiles

---
+++ src/mainApp/Bullet.java
@@ -0,0 +1,32 @@
+package mainApp;
+
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.Rectangle2D.Double;
+
+public class Bullet {
+	
+	private static final int BULLET_SIZE = 8;
+	private static final double BULLET_SPEED = 5;
+	
+	private double xPos;
+	private double yPos;
+	private double xVelocity;
+	private double yVelocity;
+	protected Rectangle2D.Double rect;
+	
+	public Bullet(double x, double y) {
+		this.xPos = x;
+		this.yPos = y;
+		this.rect = new Rectangle2D.Double(x, y, BULLET_SIZE, BULLET_SIZE);
+	}
+	
+	public void update() {
+		this.xPos += this.xVelocity;
+		this.yPos += this.yVelocity;
+	}
+	
+	public void calculateSpeed(double x, double y) {
+		
+	}
+
+}
--- src/mainApp/Jumper.java
+++ src/mainApp/Jumper.java
@@ -2,6 +2,7 @@ package mainApp;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.util.ArrayList;
 
 /**
  * Class: Jumper
@@ -15,13 +16,16 @@ public class Jumper extends Enemy {
 	private static final double JUMPER_HORI_SPEED = 2.5;
 	private static final double JUMP_SPEED = 11;
 	private static final int JUMP_COOLDOWN = 100;
+	private static final int SHOOT_COOLDOWN = 100;
 	
 	private int ticksUntilJump;
+	private ArrayList<Bullet> bullets;
 	
 	public Jumper(double x, double y) {
 		super(x, y);
 		this.xVelocity = JUMPER_HORI_SPEED;
 		this.ticksUntilJump = 0;
+		this.bullets = new ArrayList<Bullet>();
 	}
 	
 	@Override
@@ -38,6 +42,10 @@ public class Jumper extends Enemy {
 			this.yVelocity = -JUMP_SPEED;
 			this.ticksUntilJump = 0;
 		}
+	}
+	
+	public void shoot() {
+		
 	}
 //	 && this.isFalling == false
 
